6ae464ee75e48220354e63dd122cd35e21816fcb
Avinash K <kumaravi651@gmail.com>
2023-08-17 11:37:49 +0530

    Update MainView.swift
    Added to main

--- FlightApp/Presentation/MainView.swift
+++ FlightApp/Presentation/MainView.swift
@@ -14,7 +14,6 @@ struct MainView: View {
             PhoneNumberView()
             FlightBtn(buttonName: "Sign in with OTP")
             Spacer()
-            Spacer()
         }
         .padding().background(Color("Background"))
     }
